286bb6f32c37163c90dc2c6a215d2a4ac6f90f64
HippoLover123 <rebar.hamshin2003@gmail.com>
2025-11-03 16:01:25 +0100

    Fixed databases, and all dependencies

--- src/main/java/com/example/JournalApplication/model/Condition.java
+++ src/main/java/com/example/JournalApplication/model/Condition.java
@@ -1,16 +1,32 @@
 package com.example.JournalApplication.model;
 
 import jakarta.persistence.*;
-
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "[condition]")
 public class Condition {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-    @Column(nullable = false) Long patientId;
-    @Column(nullable = false) String condition;
-    String display;
-    LocalDate onsetDate;
-    Long assertedByPractitionerId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
+
+    @Column(name = "code", length = 50, nullable = false)
+    private String code;
+
+    @Column(name = "display")
+    private String display;
+
+    @Column(name = "onset_date")
+    private LocalDate onsetDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "asserted_by_practitioner_id")
+    private Practitioner assertedByPractitioner;
+
+
 }
--- src/main/java/com/example/JournalApplication/model/User.java
+++ src/main/java/com/example/JournalApplication/model/User.java
@@ -3,11 +3,13 @@ package com.example.JournalApplication.model;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
     @Column(unique = true, nullable = false) String username;
-    @Column(unique = true, nullable = false) String password;
+    @Column(name = "password_hash")
+    private String password;
     @Enumerated(EnumType.STRING) Role role; // PATIENT, DOCTOR, STAFF
 
     Long patientId;
