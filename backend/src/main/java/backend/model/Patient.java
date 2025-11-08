54e9e5df2fef2e3cc6960c109b80adf3ab11f31e
HippoLover123 <rebar.hamshin2003@gmail.com>
2025-11-08 02:28:22 +0100

    Restructure: move backend project into /backend, add /frontend

---
+++ backend/src/main/java/backend/model/Patient.java
@@ -0,0 +1,16 @@
+package backend.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(unique = true, nullable = false)
+    String personnummer;
+    String name;
+    String contactInfo;
+}
